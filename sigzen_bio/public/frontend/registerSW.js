a3ef9b974bb19461b571f15a1b3f3562db94405c
Ami-Sigzen <ami.t@sigzen.com>
2024-11-28 06:27:03 +0000

    Initial commit

---
+++ sigzen_bio/public/frontend/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/assets/sigzen_bio/frontend/sw.js', { scope: '/assets/sigzen_bio/frontend/' })})}
